272c8fd1b5a6bf1263ea3a22dd2d11d2e5e4a2c5
thebe <theberdakh@gmail.com>
2025-01-27 17:53:19 +0500

    add ProgressButton

---
+++ core/ui/src/main/java/com/aralhub/ui/ProgressButton.java
@@ -0,0 +1,96 @@
+package com.aralhub.ui;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProgressButton extends FrameLayout {
+
+    private TextView textView;
+    private ProgressBar progressBar;
+    private boolean isTapToStopEnabled;
+    private boolean isProgressStarted = false;
+
+    public ProgressButton(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ProgressButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ProgressButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public ProgressButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+        );
+        params.gravity = Gravity.CENTER;
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ProgressButton);
+        isTapToStopEnabled = a.getBoolean(R.styleable.ProgressButton_enableTapToStop, true);
+        this.setBackground(a.getDrawable(R.styleable.ProgressButton_background));
+
+        textView = new TextView(context);
+        textView.setText(a.getText(R.styleable.ProgressButton_text));
+        textView.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        textView.setTextAppearance(a.getResourceId(R.styleable.ProgressButton_textAppearance, 0));
+        addView(textView, params);
+
+        progressBar = new ProgressBar(context);
+        progressBar.setIndeterminate(true);
+        progressBar.setIndeterminateTintList(ColorStateList.valueOf(Color.WHITE));
+        progressBar.setVisibility(GONE);
+
+        textView.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        );
+        int textViewHeight = textView.getMeasuredHeight(); // to make the progress bar's height equal to the text view's height
+        LayoutParams progressBarParams = new LayoutParams(
+                textViewHeight,
+                textViewHeight
+        );
+        progressBarParams.gravity = Gravity.CENTER;
+        addView(progressBar, progressBarParams);
+
+        this.setOnClickListener(v -> {
+            isProgressStarted = !isProgressStarted;
+            if (isProgressStarted) {
+                textView.setVisibility(GONE);
+                progressBar.setVisibility(VISIBLE);
+            } else {
+                if (isTapToStopEnabled) {
+                    textView.setVisibility(VISIBLE);
+                    progressBar.setVisibility(GONE);
+                }
+            }
+        });
+
+        a.recycle();
+    }
+    public void stopProgress() {
+        textView.setVisibility(VISIBLE);
+        progressBar.setVisibility(GONE);
+    }
+
+}
