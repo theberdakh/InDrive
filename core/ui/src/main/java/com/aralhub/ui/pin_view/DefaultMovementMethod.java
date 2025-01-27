225de156b3e0d31d76a7dabf30c5b08bbeafbd02
thebe <theberdakh@gmail.com>
2025-01-27 12:48:14 +0500

    add PinView

---
+++ core/ui/src/main/java/com/aralhub/ui/pin_view/DefaultMovementMethod.java
@@ -0,0 +1,76 @@
+package com.aralhub.ui.pin_view;
+
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.method.MovementMethod;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * To disable arrow key
+ *
+ * @author Berdakh Tlepov
+ *         31/03/2018
+ */
+class DefaultMovementMethod implements MovementMethod {
+
+    private static DefaultMovementMethod sInstance;
+
+    public static MovementMethod getInstance() {
+        if (sInstance == null) {
+            sInstance = new DefaultMovementMethod();
+        }
+
+        return sInstance;
+    }
+
+    private DefaultMovementMethod() {
+    }
+
+    @Override
+    public void initialize(TextView widget, Spannable text) {
+        // It will mark the IMM as openable
+        Selection.setSelection(text, 0);
+    }
+
+    @Override
+    public boolean onKeyDown(TextView widget, Spannable text, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyUp(TextView widget, Spannable text, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onKeyOther(TextView view, Spannable text, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    public void onTakeFocus(TextView widget, Spannable text, int direction) {
+
+    }
+
+    @Override
+    public boolean onTrackballEvent(TextView widget, Spannable text, MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(TextView widget, Spannable text, MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(TextView widget, Spannable text, MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean canSelectArbitrarily() {
+        return false;
+    }
+}
