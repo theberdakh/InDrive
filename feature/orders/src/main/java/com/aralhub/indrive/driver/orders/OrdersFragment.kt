30b83808f24c6707664ea36d79ca0b758c6e460f
thebe <theberdakh@gmail.com>
2025-02-10 11:28:06 +0500

    add overview module

--- feature/orders/src/main/java/com/aralhub/indrive/driver/orders/OrdersFragment.kt
+++ feature/orders/src/main/java/com/aralhub/indrive/driver/orders/OrdersFragment.kt
@@ -1,5 +1,18 @@
 package com.aralhub.indrive.driver.orders
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.aralhub.indrive.driver.orders.databinding.FragmentOrdersBinding
+import com.aralhub.ui.utils.viewBinding
 
-class OrdersFragment: Fragment(R.layout.fragment_orders)
+class OrdersFragment : Fragment(R.layout.fragment_orders) {
+    private val binding by viewBinding(FragmentOrdersBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tbOrders.setNavigationOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+}
