bfe67adcaac010e19bb5c9b82e2f6d5c734dc022
thebe <theberdakh@gmail.com>
2025-02-10 13:13:13 +0500

    add ExitLine BottomSheet

--- feature/orders/src/main/java/com/aralhub/indrive/driver/orders/OrdersFragment.kt
+++ feature/orders/src/main/java/com/aralhub/indrive/driver/orders/OrdersFragment.kt
@@ -2,9 +2,12 @@ package com.aralhub.indrive.driver.orders
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.aralhub.indrive.driver.orders.databinding.FragmentOrdersBinding
+import com.aralhub.indrive.driver.orders.sheet.ExitLineModalBottomSheet
 import com.aralhub.ui.utils.viewBinding
 
 class OrdersFragment : Fragment(R.layout.fragment_orders) {
@@ -14,5 +17,26 @@ class OrdersFragment : Fragment(R.layout.fragment_orders) {
         binding.tbOrders.setNavigationOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
+
+        binding.tbOrders.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_exit_line -> {
+                    showExitLineBottomSheet()
+                    true
+                }
+                else -> false
+            }
+        }
+        binding.btnFilter.setOnClickListener {}
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, true) {
+            showExitLineBottomSheet()
+        }
+    }
+
+    private fun showExitLineBottomSheet() {
+        ExitLineModalBottomSheet(onExit = { findNavController().navigateUp() }).show(
+            childFragmentManager,
+            "ExitLineModalBottomSheet"
+        )
     }
 }
---
+++ feature/orders/src/main/java/com/aralhub/indrive/driver/orders/sheet/ExitLineModalBottomSheet.kt
@@ -0,0 +1,24 @@
+package com.aralhub.indrive.driver.orders.sheet
+
+import android.os.Bundle
+import android.view.View
+import com.aralhub.indrive.driver.orders.R
+import com.aralhub.indrive.driver.orders.databinding.ModalBottomSheetExitLineBinding
+import com.aralhub.ui.utils.viewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ExitLineModalBottomSheet(
+    private val onExit: () -> Unit
+): BottomSheetDialogFragment(R.layout.modal_bottom_sheet_exit_line) {
+    private val binding by viewBinding(ModalBottomSheetExitLineBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            dismissAllowingStateLoss()
+        }
+        binding.btnExit.setOnClickListener {
+            onExit.invoke()
+            dismissAllowingStateLoss()
+        }
+    }
+}
