040978cbcde56d5e05c85b47571af39a244fd395
thebe <theberdakh@gmail.com>
2025-02-13 16:49:58 +0500

    make AddLocation expanded

--- feature/request/src/main/java/com/aralhub/indrive/request/sheet/modal/AddLocationModalBottomSheet.kt
+++ feature/request/src/main/java/com/aralhub/indrive/request/sheet/modal/AddLocationModalBottomSheet.kt
@@ -9,6 +9,7 @@ import com.aralhub.indrive.request.adapter.LocationItemTouchCallback
 import com.aralhub.indrive.request.databinding.ModalBottomSheetAddLocationBinding
 import com.aralhub.indrive.request.model.LocationItem
 import com.aralhub.ui.utils.viewBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddLocationModalBottomSheet : BottomSheetDialogFragment(R.layout.modal_bottom_sheet_add_location) {
@@ -57,6 +58,14 @@ class AddLocationModalBottomSheet : BottomSheetDialogFragment(R.layout.modal_bot
     )
     private val adapter by lazy { LocationItemDraggableAdapter() }
     private val binding by viewBinding(ModalBottomSheetAddLocationBinding::bind)
+
+    override fun onStart() {
+        super.onStart()
+        val behavior = BottomSheetBehavior.from(requireView().parent as View)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        behavior.isDraggable = false
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvAddresses.adapter = adapter
