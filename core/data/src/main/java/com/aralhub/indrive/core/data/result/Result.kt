a8043045c6784f5108b9d812bd5afd67ddef9b73
thebe <theberdakh@gmail.com>
2025-03-14 09:45:10 +0500

    add fold

--- core/data/src/main/java/com/aralhub/indrive/core/data/result/Result.kt
+++ core/data/src/main/java/com/aralhub/indrive/core/data/result/Result.kt
@@ -2,11 +2,6 @@ package com.aralhub.indrive.core.data.result
 
 import com.aralhub.network.models.NetworkResult
 
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val message: String) : Result<Nothing>
-}
-
 inline fun <T, R> NetworkResult<T>.asResult(
     crossinline onSuccess: (T) -> Result<R>,
 ): Result<R> =
@@ -14,3 +9,37 @@ inline fun <T, R> NetworkResult<T>.asResult(
         is NetworkResult.Error -> Result.Error(this.message)
         is NetworkResult.Success -> onSuccess(this.data)
     }
+
+sealed interface Result<out T> {
+    data class Success<T>(val data: T) : Result<T>
+    data class Error(val message: String) : Result<Nothing>
+}
+
+inline fun <T, R> Result<T>.fold(
+    crossinline onSuccess: (T) -> R,
+    crossinline onError: (String) -> R
+): R {
+    return when (this) {
+        is Result.Success -> onSuccess(data)
+        is Result.Error -> onError(message)
+    }
+}
+
+
+inline fun <T> Result<T>.toUiState(
+    crossinline onSuccess: (T) -> UiState<T>,
+    crossinline onError: (String) -> UiState<T>
+): UiState<T> = when (this) {
+    is Result.Success -> onSuccess(data)
+    is Result.Error -> onError(message)
+}
+
+
+sealed interface UiState<out T> {
+    data class Success<out T>(val data: T) : UiState<T>
+    data object Loading
+    data class Error(val message: String)
+}
+
+
+
