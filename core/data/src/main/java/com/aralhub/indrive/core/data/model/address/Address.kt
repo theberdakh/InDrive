edeb512a3020c6aa481929cf75ad6b5fc9373404
thebe <theberdakh@gmail.com>
2025-03-14 08:27:38 +0500

    add asResult

--- core/data/src/main/java/com/aralhub/indrive/core/data/model/address/Address.kt
+++ core/data/src/main/java/com/aralhub/indrive/core/data/model/address/Address.kt
@@ -1,5 +1,8 @@
 package com.aralhub.indrive.core.data.model.address
 
+import com.aralhub.network.models.address.NetworkAddress
+import com.aralhub.network.requests.address.NetworkAddressRequest
+
 
 data class CreateAddressRequest(
     val userId: Int,
@@ -8,6 +11,25 @@ data class CreateAddressRequest(
     val latitude: Number,
     val longitude: Number
 )
+
+fun CreateAddressRequest.toNetwork() = NetworkAddressRequest(
+    this.userId,
+    this.name,
+    this.address,
+    this.latitude,
+    this.longitude
+)
+
+fun NetworkAddress.toDomain(): Address {
+    return Address(
+        this.id,
+        this.userId,
+        this.name,
+        this.address,
+        this.latitude,
+        this.longitude
+    )
+}
 
 data class Address(
     val id: Int,
--- core/data/src/main/java/com/aralhub/indrive/core/data/result/Result.kt
+++ core/data/src/main/java/com/aralhub/indrive/core/data/result/Result.kt
@@ -1,6 +1,16 @@
 package com.aralhub.indrive.core.data.result
 
+import com.aralhub.network.models.NetworkResult
+
 sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
     data class Error(val message: String) : Result<Nothing>
 }
+
+inline fun <T, R> NetworkResult<T>.asResult(
+    crossinline onSuccess: (T) -> Result<R>,
+): Result<R> =
+    when (this) {
+        is NetworkResult.Error -> Result.Error(this.message)
+        is NetworkResult.Success -> onSuccess(this.data)
+    }
