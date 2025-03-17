bf0fb130fbc984f523ea5d0836e3ca0c046d3da4
thebe <theberdakh@gmail.com>
2025-03-17 12:44:52 +0500

    add animation while waiting for offers from drivers

--- feature/offers/src/main/java/com/aralhub/offers/OffersFragment.kt
+++ feature/offers/src/main/java/com/aralhub/offers/OffersFragment.kt
@@ -1,10 +1,11 @@
 package com.aralhub.offers
 
+import android.graphics.PointF
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import com.aralhub.araltaxi.client.offers.R
 import com.aralhub.araltaxi.client.offers.databinding.FragmentOffersBinding
@@ -14,17 +15,26 @@ import com.aralhub.offers.navigation.sheet.SheetNavigator
 import com.aralhub.ui.adapter.OfferItemAdapter
 import com.aralhub.ui.utils.LifecycleOwnerEx.observeState
 import com.aralhub.ui.utils.viewBinding
+import com.dotlottie.dlplayer.Mode
+import com.lottiefiles.dotlottie.core.model.Config
+import com.lottiefiles.dotlottie.core.util.DotLottieSource
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.IconStyle
+import com.yandex.mapkit.map.RotationType
+import com.yandex.runtime.image.ImageProvider
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class OffersFragment : Fragment(R.layout.fragment_offers) {
     private val binding by viewBinding(FragmentOffersBinding::bind)
-    @Inject lateinit var navigator: SheetNavigator
-    @Inject lateinit var errorHandler: ErrorHandler
-    @Inject lateinit var featureOffersNavigation: FeatureOffersNavigation
+    @Inject
+    lateinit var navigator: SheetNavigator
+    @Inject
+    lateinit var errorHandler: ErrorHandler
+    @Inject
+    lateinit var featureOffersNavigation: FeatureOffersNavigation
     private val offerItemAdapter by lazy { OfferItemAdapter() }
     private val viewModel by activityViewModels<OffersViewModel>()
 
@@ -34,22 +44,53 @@ class OffersFragment : Fragment(R.layout.fragment_offers) {
         setUpRecyclerView()
         initBottomNavController()
         viewModel.getOffers()
+        val config = Config.Builder()
+            .autoplay(true)
+            .speed(1f)
+            .loop(true)
+            .source(DotLottieSource.Asset("pulse.json")) // file name of json/.lottie
+//            .source(DotLottieSource.Url("https://lottie.host/5525262b-4e57-4f0a-8103-cfdaa7c8969e/VCYIkooYX8.json"))
+//            .source(DotLottieSource.Url("https://lottiefiles-mobile-templates.s3.amazonaws.com/ar-stickers/swag_sticker_piggy.lottie"))
+            .playMode(Mode.FORWARD)
+            .useFrameInterpolation(true)
+            .build()
+
+        binding.dotLottie.load(config)
+        binding.dotLottie.play()
         initObservers()
     }
 
     private fun initObservers() {
-       observeState(viewModel.offersUiState){ offersUiState ->
-           when(offersUiState){
-               is OffersUiState.Error -> errorHandler.showToast(offersUiState.message)
-               OffersUiState.Loading -> {}
-               is OffersUiState.Success -> {
-                   offerItemAdapter.submitList(offersUiState.offers)
-               }
-           }
-       }
 
-        observeState(viewModel.acceptOfferUiState){ acceptOfferUiState ->
-            when(acceptOfferUiState){
+        observeState(viewModel.searchRideUiState) { searchRideUiState ->
+            when (searchRideUiState) {
+                is SearchRideUiState.Error -> {}
+                SearchRideUiState.Loading -> {}
+                is SearchRideUiState.Success -> {
+                    Log.i("Locations", "${searchRideUiState.searchRide.locations.points}")
+                    val startLocation = searchRideUiState.searchRide.locations.points[0].coordinates
+                    updateMap(
+                        startLocation.latitude,
+                        startLocation.longitude
+                    )
+                }
+            }
+        }
+
+
+        observeState(viewModel.offersUiState) { offersUiState ->
+            when (offersUiState) {
+                is OffersUiState.Error -> errorHandler.showToast(offersUiState.message)
+                OffersUiState.Loading -> {}
+                is OffersUiState.Success -> {
+                    offerItemAdapter.submitList(offersUiState.offers)
+                    binding.dotLottie.stop()
+                }
+            }
+        }
+
+        observeState(viewModel.acceptOfferUiState) { acceptOfferUiState ->
+            when (acceptOfferUiState) {
                 is AcceptOfferUiState.Error -> errorHandler.showToast(acceptOfferUiState.message)
                 AcceptOfferUiState.Loading -> {}
                 AcceptOfferUiState.Success -> {
@@ -59,12 +100,12 @@ class OffersFragment : Fragment(R.layout.fragment_offers) {
             }
         }
 
-        observeState(viewModel.declineOfferUiState){ declineOfferUiState ->
-            when(declineOfferUiState){
+        observeState(viewModel.declineOfferUiState) { declineOfferUiState ->
+            when (declineOfferUiState) {
                 is DeclineOfferUiState.Error -> errorHandler.showToast(declineOfferUiState.message)
                 DeclineOfferUiState.Loading -> {}
                 is DeclineOfferUiState.Success -> {
-                  //  offerItemAdapter.removeItem(declineOfferUiState.position)
+                    //  offerItemAdapter.removeItem(declineOfferUiState.position)
                     errorHandler.showToast("Offer declined")
                 }
             }
@@ -83,7 +124,8 @@ class OffersFragment : Fragment(R.layout.fragment_offers) {
     }
 
     private fun initBottomNavController() {
-        val navHostFragment = childFragmentManager.findFragmentById(R.id.offers_nav_host) as NavHostFragment
+        val navHostFragment =
+            childFragmentManager.findFragmentById(R.id.offers_nav_host) as NavHostFragment
         val navController = navHostFragment.navController
         navController.let {
             navigator.bind(navController)
@@ -92,14 +134,32 @@ class OffersFragment : Fragment(R.layout.fragment_offers) {
 
     private fun setUpMapView() {
         binding.mapView.onStart()
+        binding.mapView.setNoninteractive(true)
+    }
+
+    private fun updateMap(latitude: Number, longitude: Number) {
+
+        val imageProvider = ImageProvider.fromResource(context, com.aralhub.ui.R.drawable.ic_pickup_marker)
+        val placeMarkObject  = binding.mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+            geometry = Point(latitude.toDouble(), longitude.toDouble())
+            setIcon(imageProvider)
+            setIconStyle(IconStyle().apply {
+                anchor = PointF(0.5f, 0.5f)
+                scale = 0.1f
+                rotationType = RotationType.NO_ROTATION
+            })
+
+        }
+        placeMarkObject.geometry = Point(latitude.toDouble(), longitude.toDouble())
         binding.mapView.mapWindow.map.move(
             CameraPosition(
-                Point(42.4619, 59.6166),
-                /* zoom = */ 17.0f,
-                /* azimuth = */ 150.0f,
-                /* tilt = */ 30.0f
+                Point(latitude.toDouble(), longitude.toDouble()),
+                17.0f,
+                150.0f,
+                30.0f
             )
         )
+
     }
 
     override fun onDestroy() {
